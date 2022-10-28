795a54745891ad23bf35d80fee2b0e31b790eeaa
mojtabakarimi <mojtaba.karimi.mo@gmail.com>
2022-10-28 15:56:51 +0330

    feature: react and react router dom and react bootstrap added to project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Button } from "react-bootstrap";
+
+function App() {
+  return (
+    <div className="App">
+      <Button as="a" variant="success">
+        Button as link
+      </Button>
+    </div>
+  );
+}
+
+export default App;
