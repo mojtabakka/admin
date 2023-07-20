072587defe885c7a82edb7523754fe48296d6e2e
mojtabakarimi <mojtaba.karimi.mo@gmail.com>
2023-07-20 15:44:29 +0330

    feature:nothing

--- src/layout/index.js
+++ src/layout/index.js
@@ -2,17 +2,15 @@ import React from "react";
 import layoutTypes from "./comonents";
 const AppLayout = ({ children, layoutItems }) => {
   const Layout = layoutTypes.filter(
+    
     (item) => item.name.toLowerCase() === layoutItems.type.toLowerCase()
   )[0].layout;
-  
+
   return (
     <>
       <Layout>{children}</Layout>
     </>
   );
 };
-
-
-
 
 export default AppLayout;
